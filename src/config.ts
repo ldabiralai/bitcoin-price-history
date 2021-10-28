a1e7dd06910160773f550014d687b7a031a97ed5
Lawrence Dabir-Alai <lawrence@codiac.ltd>
2021-10-28 12:29:41 +0100

    enable demoing against production data locally

--- src/config.ts
+++ src/config.ts
@@ -8,11 +8,12 @@ interface ConfigEntry {
 
 const config: Config = {
   development: {
-    coindeskUrl: "http://localhost:7001/v1/bpi/historical/close.json"
+    coindeskUrl: "http://localhost:7001/v1/bpi/historical/close.json",
   },
   production: {
-    coindeskUrl: "https://api.coindesk.com/v1/bpi/historical/close.json"
-  }
+    coindeskUrl: "https://api.coindesk.com/v1/bpi/historical/close.json",
+  },
 };
 
-export default config[process.env.NODE_ENV] || config.development;
+export default config[process.env.REACT_APP_ENV || process.env.NODE_ENV] ||
+  config.development;
